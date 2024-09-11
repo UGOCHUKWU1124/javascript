af0e99b12f31c8afef6818cc3bd6244d0bf9fda1
ugochukwu <ugochukwutemitayo@gmail.com>
2024-09-12 00:01:19 +0100

    relationaloperators

---
+++ Increment and Decrement operators/id.js
@@ -0,0 +1,19 @@
+var a=10
+console.log(10);
+b = ++a;
+console.log(a,b);//output:11,11(pre increment)
+
+a=10;
+b=a++;
+console.log(a,b);//output:11,10(post increment)
+
+
+a=10;
+b=--a;//a=9 first b= 9(pre decrement)
+console.log(a,b);
+
+a=10;
+b=a--;//b=10 a=9 (post decrement)
+console.log(a,b);
+
+
