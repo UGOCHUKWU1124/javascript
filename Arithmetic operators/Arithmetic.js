03390537c7edcf66c23c67e5149e2cd7eaf96f66
ugochukwu <ugochukwutemitayo@gmail.com>
2024-09-11 17:29:08 +0100

    assignment

---
+++ Arithmetic operators/Arithmetic.js
@@ -0,0 +1,14 @@
+let a = 10,
+  b = 3;
+let c = a + b;
+let d = a - b;
+let e = a * b;
+let f = a / b;
+let g = a % b;
+let h = a**b;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
