6c6277987edb2649ab54cfbf6ca35e32621d52e6
ugochukwu <ugochukwutemitayo@gmail.com>
2024-09-11 17:53:48 +0100

    increment and decrement operators

---
+++ Assignment operators/assignment.js
@@ -0,0 +1,15 @@
+var a=10;
+ console.log(a);
+ a=100;
+ console.log(a);
+ a+=10;
+ console.log(a);
+ a-=10;
+ console.log(a);
+ a*=10;
+ console.log(a);
+ a/=10;
+ console.log(a);
+ a%=10;
+ console.log(a);
+ 
