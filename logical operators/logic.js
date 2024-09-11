d1e7487937857bd4606352ec5193a4261b247224
ugochukwu <ugochukwutemitayo@gmail.com>
2024-09-12 00:37:41 +0100

    new

---
+++ logical operators/logic.js
@@ -0,0 +1,11 @@
+var a=10,b=100,c=50;
+var d= a==b&&b>c;//false&&true
+console.log(d); //false
+
+
+var e= a==b||b>c;//false||true
+console.log(e);// true
+
+
+var f= !(a==b);//(!false)=true
+console.log(f);// true
