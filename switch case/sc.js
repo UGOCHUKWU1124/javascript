7a1041c551c42f97e10eb6ace834f5c8dd32dd3b
ugochukwu <ugochukwutemitayo@gmail.com>
2024-09-19 19:22:31 +0100

    new

---
+++ switch case/sc.js
@@ -0,0 +1,10 @@
+//check color 
+var color="pink";
+switch (color)
+{
+    case "red":console.log("color is red");break;
+    case "green":console.log("color is green");break;
+    case "yellow":console.log("yellow is nice ");break;
+    case "blue":console.log("you love blue");break;
+    default:console.log("No color matches")
+}
