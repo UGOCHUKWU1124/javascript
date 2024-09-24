859cb4211d6fc41ff076e0b89a1e840161d939c0
ugochukwu <ugochukwutemitayo@gmail.com>
2024-09-24 14:24:12 +0100

    new

---
+++ For/for.js
@@ -0,0 +1,15 @@
+//incremented loop for one to 5 numbers
+console.log("incremented loop");
+for (var i = 1; i <= 5; i++)
+{
+  console.log(i);
+}
+
+//decremented loop from 7 to 1 numbers
+console.log("decremented loop");
+for (var i = 7; i >= 1; i--)
+{
+  console.log(i);
+}
+
+
