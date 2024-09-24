8d7a8ae0469f8372c1d17eae17459090ff6fe656
ugochukwu <ugochukwutemitayo@gmail.com>
2024-09-24 13:24:28 +0100

    new

---
+++ do-while/do-while.js
@@ -0,0 +1,14 @@
+//do-while:print 1 to 5
+//increment loop
+var i=1;
+do {
+  console.log(i);
+  i++;
+} while (i<= 5);
+
+//decrement loop
+var i=10;
+do {
+  console.log(i);
+  i--;
+} while (i>= 1);
