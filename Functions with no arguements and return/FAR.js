b8cebc10af78e3f226f80ff8a678a0386a4cb9ef
ugochukwu <ugochukwutemitayo@gmail.com>
2024-10-20 23:24:25 +0100

    new

---
+++ Functions with no arguements and return/FAR.js
@@ -0,0 +1,9 @@
+//functions with no arguments and no return value 
+function showCity() 
+  // instead of saying let showCity = function()
+{
+  console.log("New York");
+}
+showCity();
+showCity();
+showCity();
